bc689834857347e9acf488653f3a74ce3dfc0297
jonathansalinas <jonathan.salinas05@utrgv.edu>
2022-03-28 18:20:15 -0500

    stuff

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import numpy as np
 import pandas as pd
 
 
-
 # Data
 # Growth between 1960 to ~2018
 countryLandTot = pd.read_csv("API_AG.LND.TOTL.K2_DS2_en_csv_v2_511817.csv")
+# Population
 countryPopTot = pd.read_csv("API_SP.POP.TOTL_DS2_en_csv_v2_511378.csv")
 # Countries GDP USD
 countryGDPTotUSD = pd.read_csv("API_NY.GDP.MKTP.CD_DS2_en_csv_v2_559588.csv")
-# Military Expenditure Percentage
+# Military Expenditure Percentage ???
 countryMilExp = pd.read_csv("API_MS.MIL.XPND.GD.ZS_DS2_en_csv_v2_511529.csv")
-
+# Military Total
+countryMilTot = pd.read_csv("API_MS.MIL.TOTL.P1_DS2_en_csv_v2_3731281.csv")
 # Current
 ######################
 
