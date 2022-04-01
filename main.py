0a14deaf7293e20895e86217b0e186e9ae8d97c8
jonathansalinas <jonathan.salinas05@utrgv.edu>
2022-04-01 10:46:32 -0500

    Updated Files

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 import numpy as np
 import pandas as pd
 
-# Data
-# Growth between 1960 to ~2018
-countryLandTot = pd.read_csv("API_AG.LND.TOTL.K2_DS2_en_csv_v2_511817.csv")
-# Population
-countryPopTot = pd.read_csv("API_SP.POP.TOTL_DS2_en_csv_v2_511378.csv")
-# Countries GDP USD
-countryGDPTotUSD = pd.read_csv("API_NY.GDP.MKTP.CD_DS2_en_csv_v2_559588.csv")
-# Military Expenditure Percentage ???
-countryMilExp = pd.read_csv("API_MS.MIL.XPND.GD.ZS_DS2_en_csv_v2_511529.csv")
-# Military Total
-countryMilTot = pd.read_csv("API_MS.MIL.TOTL.P1_DS2_en_csv_v2_3731281.csv")
+
+militaryExpenditure_TL = pd.read_csv("Military_Expenditure_1960-2019.csv")
+population_TL = pd.read_csv("Population_1960-2019.csv")
+gdp_TL = pd.read_csv("GDP_1960-2019.csv")
+
+print(gdp_TL)
 
 
-# Current
-######################
+
+
+
+
+
+####################################################################################################
+#  OLD Data
 
 # Gets...cell (Row x Column)
-dataPop = countryPopTot.loc[countryPopTot['Country Name'] == 'United States']
+#dataPop = countryPopTot.loc[countryPopTot['Country Name'] == 'United States']
 # Row's Column (cell)
-dataYear = dataPop.iloc[0]['2018']
-print(dataPop) # Row data
-print(dataYear) # cell
-
-
+#dataYear = dataPop.iloc[0]['2018']
+#print(dataPop) # Row data
+#print(dataYear) # cell
 # Ukraine(246), United States(249), Russia(200), China(38), India(107)
 # Print
-print(countryLandTot.iloc[246])
-print(countryPopTot.iloc[38])
-print(countryMilExp.iloc[249])
+#print(countryLandTot.iloc[246])
+#print(countryPopTot.iloc[38])
+#print(countryMilExp.iloc[249])
+
+################################################################################################################
