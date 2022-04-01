1a7d1e99b513bf670b568c55b696d72b6340735c
jonathansalinas <jonathan.salinas05@utrgv.edu>
2022-04-01 11:33:23 -0500

    GDP

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import stat
-import math
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 
-def PlotCountry(country, color, num):
+def PlotCountry(country, color, num, topic):
     ##### DATA EXTRACTION/MINIPULATION #####
     countryPlot = plt
     plotTitle = country.iloc[0][0]
@@ -19,13 +17,13 @@ def PlotCountry(country, color, num):
     # print(countryGDP_TL)
 
     ##### PLOT #####
-    countryPlot.figure(num) # COMMENT OUT IF YOU WANT TO SHOW MULTIPLE COUNTRIES IN A SINGLE FIGURE
+    #countryPlot.figure(num) # COMMENT OUT IF YOU WANT TO SHOW MULTIPLE COUNTRIES IN A SINGLE FIGURE
     countryPlot.plot(countryGDP_TL['Year'], countryGDP_TL['Value'], color= color, label = plotTitle)
     # countryPlot.legend(plotTitle) # DOESNT WORK FOR IF YOU HAVE MULTIPLE FIGURE IN ONE
-    countryPlot.title(plotTitle + ' GDP')
+    countryPlot.title(plotTitle + ' ' + topic)
     countryPlot.xlabel("Year")
     countryPlot.xlim(1960, 2019)
-    countryPlot.ylabel("GDP (US$)")
+    countryPlot.ylabel( topic + " (US$)")
     return countryPlot
 
 
@@ -37,26 +35,57 @@ militaryExpenditure_TL = pd.read_csv("Military_Expenditure_1960-2019.csv")
 population_TL = pd.read_csv("Population_1960-2019.csv")
 gdp_TL = pd.read_csv("GDP_1960-2019.csv")
 
+### NOTE* BETTER VARIABLE DISTRUBUTION E.g. Country Object ({data, color, Name,})
+
+    ####### GLOBAL VALUES ######## (might change)
+gdp_Color = 'red'
+militaryExpediture_Color = 'black'
+
     ########## COUNTRIES GDP ##########
 
     ###### USA GDP GROWTH ######
 usaGDP_TL = gdp_TL.loc[gdp_TL['Country Name'] == 'United States']
-PlotCountry(usaGDP_TL, 'blue', 1)
+PlotCountry(usaGDP_TL, gdp_Color, 1, 'GDP')
 
     ###### CHINA GDP GROWTH ######
 chinaGDP_TL = gdp_TL.loc[gdp_TL['Country Name'] == 'China']
-PlotCountry(chinaGDP_TL, 'red', 2)
+PlotCountry(chinaGDP_TL, gdp_Color, 2, 'GDP')
 
     ###### RUSSIAN GDP GROWTH ######
 russiaGDP_TL = gdp_TL.loc[gdp_TL['Country Name'] == 'Russian Federation']
-PlotCountry(russiaGDP_TL, 'black', 3)
+PlotCountry(russiaGDP_TL, gdp_Color, 3, 'GDP')
 
     ###### INDIA GDP GROWTH ######
 indiaGDP_TL = gdp_TL.loc[gdp_TL['Country Name'] == 'India']
-PlotCountry(indiaGDP_TL, 'orange', 4)
+PlotCountry(indiaGDP_TL, gdp_Color, 4, 'GDP')
 
     ###### GERMANY GDP GROWTH ######
 germanyGDP_TL = gdp_TL.loc[gdp_TL['Country Name'] == 'Germany']
-PlotCountry(germanyGDP_TL, 'yellow', 5)
+PlotCountry(germanyGDP_TL, gdp_Color, 5, 'GDP')
+
+plt.show()
+
+    ############## Military Expenditures ##################
+
+    ###### USA MILITARY EXPENDITURES GROWTH ######
+
+usaME_TL = militaryExpenditure_TL.loc[militaryExpenditure_TL['Country Name'] == 'United States']
+PlotCountry(usaGDP_TL, militaryExpediture_Color, 1, 'Military Expenditures')
+
+    ###### CHINA MILITARY EXPENDITURES GROWTH ######
+chinaME_TL = militaryExpenditure_TL.loc[militaryExpenditure_TL['Country Name'] == 'China']
+PlotCountry(chinaGDP_TL, militaryExpediture_Color, 2, 'Military Expenditures')
+
+    ###### RUSSIAN MILITARY EXPENDITURES GROWTH ######
+russiaME_TL = militaryExpenditure_TL.loc[militaryExpenditure_TL['Country Name'] == 'Russian Federation']
+PlotCountry(russiaGDP_TL, militaryExpediture_Color, 3, 'Military Expenditures')
+
+    ###### INDIA GDP GROWTH ######
+indiaME_TL = militaryExpenditure_TL.loc[militaryExpenditure_TL['Country Name'] == 'India']
+PlotCountry(indiaGDP_TL, militaryExpediture_Color, 4, 'Military Expenditures')
+
+    ###### GERMANY MILITARY EXPENDITURES GROWTH ######
+germanyME_TL = militaryExpenditure_TL.loc[militaryExpenditure_TL['Country Name'] == 'Germany']
+PlotCountry(germanyGDP_TL, militaryExpediture_Color, 5, 'Military Expenditures')
 
 plt.show()
